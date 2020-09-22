3d069273abe9ec327e0a8281ac0aeef57b052d9a
Diogo Barradas <58223429+diogo-barradas@users.noreply.github.com>
2020-09-22 17:13:59 +0100

    update

---
+++ rotinas.js
@@ -0,0 +1,7 @@
+//mudar background 
+        var r = randomIntFromInterval(0,255);
+        var g = randomIntFromInterval(0,255);
+        var b = randomIntFromInterval(0,255);
+        var backColor = "rgb(" + r + "," + g + "," + b + ")";
+
+        document.bgColor = backColor;
