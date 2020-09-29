e9ffd20bc0a74b53bdae3f3ef8f128279f157ee6
Diogo Barradas <58223429+diogo-barradas@users.noreply.github.com>
2020-09-29 14:55:40 +0100

    dia 29

--- rotinas.js
+++ rotinas.js
@@ -1,6 +1,21 @@
-//mudar background 
+// mudar background 
         var r = randomIntFromInterval(0,255);
         var g = randomIntFromInterval(0,255);
         var b = randomIntFromInterval(0,255);
         var backColor = "rgb(" + r + "," + g + "," + b + ")";
 
+// compoe o valor para uma cor no formato RGB
+function getRGBColor(red, green, blue)
+{
+        return "rgb("+ red + "," + green + "," + blue +")";
+}
+
+// gera uma cor RGB de forma aleatoria 
+function getRandomRGBColor()
+{
+        var r = randomIntFromInterval(0,255);
+        var g = randomIntFromInterval(0,255);
+        var b = randomIntFromInterval(0,255);
+
+        return getRGBColor(r,g,b);
+}
